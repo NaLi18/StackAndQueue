5f6e298c6469c9bd10c4eeeb907281a8f9aa4b45
Na Li <nli@albany.edu>
2017-04-02 17:30:06 -0400

    Complete the push method of the Stack class

--- Stack.java
+++ Stack.java
@@ -3,30 +3,13 @@ public class Stack{
   public Stack(){
     top = null;
   }
- public void push(int index, String str){
-    Node newNode = new Node(str);
-      if(this.isEmpty()){
-        this.top = newNode;
-      }
-      //add to the fornt of node list
-      else if (index == 0) {
-        newNode.setNext(top);
-        top.setPrev(newNode);
-        this.top = newNode;
-      }
-      //add to the end of node list
-        Node temp = this.top;
-        for(int i = 0; i < index - 1; i++) {
-          temp = temp.getNext();
-        }
-        Node temp2 = temp.getNext();
-        temp.setNext(newNode);
-        newNode.setPrev(temp);
-        newNode.setNext(temp2);
-        temp2.setPrev(newNode); 
-  }
+ public void push(String str){
+   Node newNode = new Node(str);
+   newNode.setNext(top);
+   this.top = newNode; 
+ }
   
-  public Node pop(Node node){
+ public Node pop(Node node){
     //return and delete the top node of the stack
    return this.top;
 
