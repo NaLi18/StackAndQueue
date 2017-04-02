35b01f1cefd59f465f7c71190510cd944fd7cb66
Na Li <nli@albany.edu>
2017-04-02 01:17:57 -0400

    Creat a new Queue class and copy all the staff of the Stack class

---
+++ Queue.java
@@ -0,0 +1,43 @@
+public class Queue{private Node top;
+  public Queue(){
+    top = null;
+  }
+ public void push(int index, String str){
+    Node newNode = new Node(str);
+      if(this.isEmpty()){
+        this.top = newNode;
+      }
+      //add to the fornt of node list
+      else if (index == 0) {
+        newNode.setNext(top);
+        top.setPrev(newNode);
+        this.top = newNode;
+      }
+      //add to the end of node list
+        Node temp = this.top;
+        for(int i = 0; i < index - 1; i++) {
+          temp = temp.getNext();
+        }
+        Node temp2 = temp.getNext();
+        temp.setNext(newNode);
+        newNode.setPrev(temp);
+        newNode.setNext(temp2);
+        temp2.setPrev(newNode); 
+  }
+  
+  public Node pop(Node node){
+    //return and delete the top node of the stack
+   return this.top;
+
+  }
+  public Node peek(){
+    return this.top;
+  }
+  public void destory(){
+    top = null;
+  }
+  
+ public boolean isEmpty() {
+    return this.top==null;
+ } 
+}
