13ac50ee2b10f6d400b52a7d2ad455a0a1511efc
Na Li <nli@albany.edu>
2017-04-04 13:41:06 -0400

    complete the enqueue method of Queue class

--- Queue.java
+++ Queue.java
@@ -5,28 +5,17 @@ public class Queue{
     top = null;
     earlyInput = null;
   }
- public void enqueue(int index, String str){
+ public void enqueue(String str){
     Node newNode = new Node(str);
       if(this.isEmpty()){
         this.top = newNode;
+        earlyInput = newNode;
       }
-      //add to the fornt of node list
-      else if (index == 0) {
+      else{
         newNode.setNext(top);
-        top.setPrev(newNode);
-        this.top = newNode;
+        top = newNode;
       }
-      //add to the end of node list
-        Node temp = this.top;
-        for(int i = 0; i < index - 1; i++) {
-          temp = temp.getNext();
-        }
-        Node temp2 = temp.getNext();
-        temp.setNext(newNode);
-        newNode.setPrev(temp);
-        newNode.setNext(temp2);
-        temp2.setPrev(newNode); 
-  }
+ }
   
   public Node dequeue(Node node){
     //return and delete the top node of the stack
